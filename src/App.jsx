055e9520503a4daaa55d5c772e45e56ecc0753c5
htikkke <htikew574@gmail.com>
2026-08-30 17:27:46 +0630

    import calculation and result container.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import CalculationContainer from "./CalculationContainer";
+import ResultContainer from "./ResultContainer";
+
 export default function App() {
   return (
     <div
       id="main-container"
       className="w-full max-w-2xl bg-white rounded-2xl p-6"
-    ></div>
+    >
+      <CalculationContainer />
+      <ResultContainer />
+    </div>
   );
 }
