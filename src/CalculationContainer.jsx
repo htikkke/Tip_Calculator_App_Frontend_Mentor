04354fe8fd1a87af4c3713dd735c147a99226c04
htikkke <htikew574@gmail.com>
2026-08-31 10:55:47 +0630

    build number of people container.

--- src/CalculationContainer.jsx
+++ src/CalculationContainer.jsx
@@ -1,11 +1,13 @@
 import BillInput from "./BillInput.jsx";
 import SelectTipContainer from "./SelectTipContainer.jsx";
+import NopInput from "./NopInput.jsx";
 
 export default function CalculationContainer() {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-8">
       <BillInput />
       <SelectTipContainer />
+      <NopInput />
     </div>
   );
 }
