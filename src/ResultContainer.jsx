021aae025a65b2852a501d1a2be5ded20289d17b
htikkke <htikew574@gmail.com>
2026-08-31 11:23:36 +0630

    build total amount.

--- src/ResultContainer.jsx
+++ src/ResultContainer.jsx
@@ -1,9 +1,11 @@
 import TipAmount from "./TipAmount";
+import TotalAmount from "./TotalAmount";
 
 export default function ResultContainer() {
   return (
     <div className="flex flex-col gap-8 bg-custom-green-900 p-4 rounded-2xl col-start-2">
       <TipAmount />
+      <TotalAmount />
     </div>
   );
 }
---
+++ src/TotalAmount.jsx
@@ -0,0 +1,15 @@
+export default function TotalAmount() {
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        <p className="font-space-mono text-custom-gray-50 font-bold text-sm">
+          Total Amount
+        </p>
+        <p className="text-custom-gray-400 font-space-mono text-xs">/ person</p>
+      </div>
+      <p className="text-custom-green-400 font-bold text-2xl font-space-mono">
+        $0.00
+      </p>
+    </div>
+  );
+}
