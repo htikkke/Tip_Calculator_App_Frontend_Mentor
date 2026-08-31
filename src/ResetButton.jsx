195ef4b6b6315d234735c6544ef78a6535abf4ef
htikkke <htikew574@gmail.com>
2026-08-31 11:33:13 +0630

    build reset button.

---
+++ src/ResetButton.jsx
@@ -0,0 +1,7 @@
+export default function ResetButton() {
+  return (
+    <button className="uppercase font-space-mono bg-[#0d686d] text-[#084d51] rounded-lg py-2 px-4 font-bold">
+      Reset
+    </button>
+  );
+}
--- src/ResultContainer.jsx
+++ src/ResultContainer.jsx
@@ -1,11 +1,15 @@
 import TipAmount from "./TipAmount";
 import TotalAmount from "./TotalAmount";
+import ResetButton from "./ResetButton";
 
 export default function ResultContainer() {
   return (
-    <div className="flex flex-col gap-8 bg-custom-green-900 p-4 rounded-2xl col-start-2">
-      <TipAmount />
-      <TotalAmount />
+    <div className="bg-custom-green-900 p-6 rounded-2xl col-start-2 flex flex-col justify-between">
+      <div className="flex flex-col gap-8">
+        <TipAmount />
+        <TotalAmount />
+      </div>
+      <ResetButton />
     </div>
   );
 }
