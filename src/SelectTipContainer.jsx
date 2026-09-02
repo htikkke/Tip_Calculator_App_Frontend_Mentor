3657812d829c0e1dc3d3068eedf8fdfa5e044d10
htikkke <htikew574@gmail.com>
2026-09-02 16:50:42 +0630

    fix custom-input.

--- src/SelectTipContainer.jsx
+++ src/SelectTipContainer.jsx
@@ -25,9 +25,11 @@ export default function SelectTipContainer() {
         <button className="bg-custom-green-900 rounded-lg py-2 text-white font-bold font-space-mono cursor-pointer hover:bg-custom-green-400 hover:text-custom-green-900 transition-all">
           50%
         </button>
-        <button className="bg-slate-100/70 rounded-lg py-2 text-custom-gray-500 font-bold font-space-mono cursor-pointer hover:bg-custom-green-400 hover:text-custom-green-900 transition-all">
-          Custom
-        </button>
+        <input
+          type="text"
+          placeholder="Custom"
+          className="w-full bg-slate-100/70 text-cyan-900 font-bold font-space-mono text-lg text-right py-2 px-3 rounded-lg outline-none focus:ring-2 focus:ring-emerald-500 placeholder:text-gray-500 placeholder:text-center transition-all cursor-pointer"
+        />
       </div>
     </div>
   );
