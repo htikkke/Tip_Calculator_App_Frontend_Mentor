919fed65f01796fbf0219df9e4fd998d8c44df6a
htikkke <htikew574@gmail.com>
2026-09-03 09:11:37 +0630

    add error state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
+import { useState, createContext } from "react";
 import CalculationContainer from "./CalculationContainer";
 import ResultContainer from "./ResultContainer";
 
+export const FormContext = createContext();
+
 export default function App() {
+  const [isReset, setIsReset] = useState(false);
+  const [nopInput, setNopInput] = useState("");
+
   return (
-    <>
-      <p className="uppercase font-space-mono font-bold text-custom-green-900 tracking-[10px]">
-        Spli
-        <br />
-        tter
-      </p>
-      <div
-        id="main-container"
-        className="w-full max-w-2xl mt-16 bg-white rounded-2xl p-6 grid grid-cols-1 md:grid-cols-2 gap-8"
-      >
-        <CalculationContainer />
-        <ResultContainer />
+    <FormContext.Provider
+      value={{ isReset, setIsReset, nopInput, setNopInput }}
+    >
+      <div className="w-full min-h-screen bg-custom-gray-200 flex flex-col items-center justify-center">
+        <p className="uppercase font-space-mono font-bold text-custom-green-900 tracking-[10px]">
+          Spli
+          <br />
+          tter
+        </p>
+        <div
+          id="main-container"
+          className="w-full max-w-2xl mt-16 bg-white rounded-2xl p-6 grid grid-cols-1 md:grid-cols-2 gap-8"
+        >
+          <CalculationContainer />
+          <ResultContainer />
+        </div>
       </div>
-    </>
+    </FormContext.Provider>
   );
 }
--- src/NopInput.jsx
+++ src/NopInput.jsx
@@ -1,19 +1,33 @@
+import { useContext } from "react";
+import { FormContext } from "./App";
+
 export default function NopInput() {
+  const { nopInput, setNopInput } = useContext(FormContext);
+
   return (
     <div className="flex flex-col">
       {/* label */}
-      <label className="font-space-mono font-bold text-custom-gray-400 mb-2">
-        Number Of People
-      </label>
+      <div className="flex items-center justify-between">
+        <label className="font-space-mono font-bold text-custom-gray-400 mb-2 text-sm">
+          Number Of People
+        </label>
+        {/* error-state */}
+        <label
+          className={`${nopInput === "0" ? "block" : "hidden"} font-space-mono font-bold text-red-600/70 mb-2 text-sm`}
+        >
+          Can't be zero
+        </label>
+      </div>
       {/* input-container */}
       <div className="relative flex items-center">
         <span className="absolute left-4 font-space-mono text-custom-gray-400 text-xl">
           <img src="images/icon-person.svg" />
         </span>
         <input
+          onChange={(e) => setNopInput(e.target.value)}
           type="text"
           placeholder="0"
-          className="bg-slate-100/70 outline-none w-full font-space-mono text-right text-custom-gray-400 font-bold px-4 py-2 rounded-lg"
+          className={`bg-slate-100/70 outline-none w-full font-space-mono text-right text-custom-gray-400 font-bold px-4 py-2 rounded-lg ${nopInput === "0" ? "border-2" : ""} ${nopInput === "0" ? "border-red-600" : ""}`}
         />
       </div>
     </div>
