72dc12602931ae71a00d89e168cc5ce51e8d7f80
htikkke <htikew574@gmail.com>
2026-09-04 09:35:54 +0630

    Deploy project to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Tip_Calculator_App_Frontend_Mentor/",
+});
